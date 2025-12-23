19426da1df5c8307a4bfc4dd6f3574c5136f990f
Zhengqiang Liao <lzq666amn@gmail.com>
2025-12-23 14:14:53 +0800

    Push

--- deepul/models/transformer/utils.py
+++ deepul/models/transformer/utils.py
@@ -1,5 +1,5 @@
 import math
-from einops import rearrange
+from einops import einsum, rearrange
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,8 +8,9 @@ from typing import Any, Iterable, Optional, Tuple, Union
 from jaxtyping import Float, Int
 
 from deepul.models.attention.utils import RotaryCausalSelfAttention, CausalSelfAttention
-from deepul.models.modules.layers import Linear, Embedding, LayerNorm, FFN, RMSNorm, SwiGLU
-from transformers import ImageGPTModel
+from deepul.models.modules.layers import Linear, Embedding, LayerNorm, FFN, RMSNorm, SwiGLU, PositionalEncoding1D
+import numpy as np
+import numpy.typing as npt
 
 __all__ = [
     "GPT",
@@ -82,13 +83,21 @@ class IGPT(nn.Module):
                 d_model: int, 
                 num_heads: int,
                 d_ff: int,
+                max_seq_len: int,
+                device=None,
+                dtype=None,
                 ) -> None:
+        factory_kwargs = {"device": device, "dtype": dtype}
         super().__init__()
         self.vocab_size = vocab_size
         
         self.num_layers = num_layers
         
-        self.embedding = Embedding(num_embeddings=vocab_size, embedding_dim=d_model)
+        self.d_model = d_model
+        
+        self.pos_encoding = PositionalEncoding1D(d_model, max_len=max_seq_len, device=device)
+        
+        self.embedding = Embedding(num_embeddings=vocab_size, embedding_dim=d_model, **factory_kwargs)
 
         self.layers = nn.ModuleList(
             [
@@ -96,18 +105,23 @@ class IGPT(nn.Module):
                     d_model=d_model,
                     num_heads=num_heads,
                     d_ff=d_ff,
+                    **factory_kwargs,
                 )
                 for _ in range(num_layers)
             ]
         )
 
-        self.ln_final = LayerNorm(d_model=d_model)
-        self.lm_head = Linear(in_features=d_model, out_features=vocab_size)
+        self.ln_final = LayerNorm(d_model=d_model, **factory_kwargs)
+        self.lm_head = Linear(in_features=d_model, out_features=vocab_size, **factory_kwargs)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         _, sequence_length = x.size()
 
         x = self.embedding(x)
+        
+        x = x * math.sqrt(self.d_model)
+        
+        x = self.pos_encoding(x)
         
         for layer in self.layers:
             x = layer(x)
@@ -117,10 +131,45 @@ class IGPT(nn.Module):
         return self.lm_head(x)
     
     def loss(self, x: torch.Tensor) -> torch.Tensor:
-        raise NotImplementedError
+        x = x.to(next(self.parameters()).device)
+        
+        inputs = x[:, :-1] # (B, T-1)
+        targets = x[:, 1:] # (B, T-1)
+        
+        logits = self(inputs) # (B, T-1, V)
+        
+        B, T, V = logits.shape
+        
+        logits = logits.reshape(B * T, V) # (B*T, V)
+        targets = targets.reshape(B * T) # (B*T)
+        
+        return F.cross_entropy(logits, targets) # ignore_index = -100
     
-    def sample(self) -> torch.Tensor:
-        raise NotImplementedError
+    @torch.no_grad()
+    def sample(self, n_samples: int, image_shape: tuple, temperature=1.0) -> torch.Tensor:
+        device = next(self.parameters()).device
+        C, H, W = image_shape
+        total_pixels = C * H * W
+
+        x = torch.zeros((n_samples, 1), dtype=torch.long, device=device)
+        
+        for _ in range(total_pixels):
+            cond = x if x.size(1) <= self.pos_encoding.max_len else x[:, -self.pos_encoding.max_len:]
+
+            logits = self(cond) # output shape: (B, T, vocab_size)
+
+            next_token_logits = logits[:, -1, :] / temperature
+
+            probs = F.softmax(next_token_logits, dim=-1)
+            next_token = torch.multinomial(probs, num_samples=1) # (B, 1)
+
+            x = torch.cat((x, next_token), dim=1)
+
+        output_seq = x[:, 1:] 
+        
+        output_image = output_seq.view(n_samples, H, W, C)
+
+        return output_image.permute(0, 3, 1, 2)
 
 
 class MMGPT(nn.Module):
@@ -201,7 +250,8 @@ class IGPTBlock(nn.Module):
         
         self.self_attn = CausalSelfAttention(
             d_model=d_model,
-            num_heads=num_heads
+            num_heads=num_heads,
+            **factory_kwargs,
         )
         
         self.layer_norm2 = LayerNorm(d_model, **factory_kwargs)
@@ -239,3 +289,16 @@ def gradient_cliping(parameters: Iterable[nn.Parameter], max_l2_norm: float, eps
             if p.grad is None:
                 continue
             p.grad.data.mul_(scale)
+
+def get_batch(dataset: npt.NDArray, batch_size: int, context_length: int, device: str):
+    starting_idxs = torch.randint(len(dataset) - context_length, (batch_size,))
+    x = torch.stack([torch.from_numpy((dataset[i:i+context_length]).astype(np.int64)) for i in starting_idxs])
+    y = torch.stack([torch.from_numpy((dataset[i+1:i+1+context_length]).astype(np.int64)) for i in starting_idxs])
+    # if "cuda" in device: passed
+    if device.type == "cuda":
+        x = x.pin_memory().to(device, non_blocking=True)
+        y = y.pin_memory().to(device, non_blocking=True)
+    else:
+        x = x.to(device)
+        y = y.to(device)
+    return x, y
