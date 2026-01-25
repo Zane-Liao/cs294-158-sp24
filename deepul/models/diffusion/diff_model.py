3988553388204dcf579b7db0735f9b09d5abfcff
Zhengqiang Liao <lzq666amn@gmail.com>
2026-01-25 11:48:35 +0800

    Fix Bug

--- deepul/models/diffusion/diff_model.py
+++ deepul/models/diffusion/diff_model.py
@@ -38,7 +38,7 @@ class GaussianDiffusion(nn.Module):
         assert not (type(self) == GaussianDiffusion and self.model.in_dim != self.model.out_dim)
         assert not hasattr(self.model, 'random_or_learned_sinusoidal_cond') or not self.model.random_or_learned_sinusoidal_cond
         
-        self.dim = getattr(self.model, 'in_dim', self.model.dim)
+        self.dim = getattr(self.model, 'in_dim', self.model.dim) or getattr(self.model, 'init_dim', self.model.dim)
         self.self_condition = self.model.self_condition
         
         self.size = size
